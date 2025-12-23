824a45dee6603d8f0d6916ad3edfe0cb1a7e7f61
ritesh <riteshpatidar088@gmail.com>
2025-12-23 12:16:03 +0530

    updated tillthe aggregation

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Credentials', 'true');
   // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type '
+  );
   next();
 });
 
